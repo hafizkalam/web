623606049d0f56c5e7ba0869a88dcc98678ea906
hafizkalam <hafizkalamabdillah@gmail.com>
2023-06-03 13:29:01 +0700

    Create fitur add user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function show()
+    {
+        $data['data'] = User::get();
+
+        return view('admin.user', $data);
+    }
+
+    public function createedit(Request $request)
+    {
+
+        $vaUpdate = array(
+            "id" => $request->id,
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make("123"),
+            "level" => $request->level,
+
+        );
+
+        if ($request->has('edit')) {
+            User::where('id', $request->id)->update($vaUpdate);
+        } else {
+            User::create($vaUpdate);
+            $request->session()->put('notif', "Data berhasil ditambahkan");
+        }
+
+        return redirect('user');
+    }
+
+    public function destory($id)
+    {
+        User::where('id', $id)->delete();
+
+        return redirect('user');
+    }
+}
---
+++ resources/views/admin/user.blade.php
@@ -0,0 +1,183 @@
+@extends('admin.admin')
+
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Website</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Website</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#modal-tambah">
+                          Tambah
+                        </button>
+
+                      </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th>Level</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $value)
+                                <tr>
+                                    <td>{{ $value->id }}</td>
+                                    <td>{{ $value->name }}</td>
+                                    <td>{{ $value->email }}</td>
+                                    <td>{{ $value->level }}</td>
+                                    <td>
+                                        <a href="userdelete/{{ $value->id }}" class="btn btn-danger float-right">Delete</a>
+
+                                        <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#modal-edit">
+                                            Edit
+                                        </button>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+
+<div class="modal fade" id="modal-tambah">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <form method="POST" enctype='multipart/form-data' action="user">
+          @csrf
+          <div class="modal-header">
+            <h4 class="modal-title">Tambah User</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-1 col-form-label">Nama</label>
+                <div class="col-sm-11">
+                  <input type="text" name="name" class="form-control">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-1 col-form-label">Email</label>
+                <div class="col-sm-11">
+                  <input type="email" name="email" class="form-control">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-1 col-form-label">Level</label>
+                <div class="col-sm-11">
+                  <input type="text" name="level" class="form-control">
+                </div>
+              </div>
+              {{-- <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-1 col-form-label">Password</label>
+                <div class="col-sm-11">
+                  <input type="password" name="password" class="form-control">
+                </div>
+              </div> --}}
+            </div>
+          <div class="modal-footer justify-content-between bg-light">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save </button>
+          </div>
+
+        </form>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+
+  <div class="modal fade" id="modal-edit">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <form method="POST" id="saveEdit" enctype='multipart/form-data' action="user">
+          @csrf
+          <div class="modal-header">
+            <h4 class="modal-title">Edit User</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group row">
+              <label for="inputEmail3" class="col-sm-1 col-form-label">Nama</label>
+              <div class="col-sm-11">
+                <input type="text" name="name" id="name" class="form-control">
+                <input type="text" name="id" id="id" class="d-none">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="inputEmail3" class="col-sm-1 col-form-label">Email</label>
+              <div class="col-sm-11">
+                <input type="email" name="email" id="email" class="form-control">
+                <input type="email" name="id" id="id" class="d-none">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="inputEmail3" class="col-sm-1 col-form-label">Level</label>
+              <div class="col-sm-11">
+                <input type="text" name="level" id="level" class="form-control">
+                <input type="text" name="id" id="id" class="d-none">
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer justify-content-between bg-light">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" name="edit" class="btn btn-primary">Save </button>
+          </div>
+
+        </form>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+@endsection
+
+@section('script')
+<script>
+    $("#example1").DataTable();
+    $('#example1 tbody').on('click', 'tr', function() {
+        var table = $('#example1').DataTable();
+       var data = table.row(this).data();
+       $("#name").val(data[1]);
+       $("#email").val(data[2]);
+       $("#level").val(data[3]);
+    // alert(data[1]);
+   });
+</script>
+@endsection
