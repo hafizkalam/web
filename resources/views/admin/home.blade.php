130e2a5abc578c79e8a2ab2b5a17be91a8044dfe
hafizkalam <hafizkalamabdillah@gmail.com>
2023-06-18 17:38:17 +0700

    edit content home admin page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,27 +2,29 @@
 
 @section('content')
 <div class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1 class="m-0">Dashboard</h1>
-        </div><!-- /.col -->
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Dashboard</li>
-          </ol>
-        </div><!-- /.col -->
-      </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-  </div>
-  <!-- /.content-header -->
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Dashboard</h1>
+      </div><!-- /.col -->
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
 
-  <!-- Main content -->
-  <section class="content">
-    <div class="callout callout-info">
-      <h5>Selamat Datang!</h5>
-      <p>Sistem Informasi Pemesanan Makanan Berbasis Website</p>
-    </div>
-  </section>
+<!-- Main content -->
+<section class="content">
+  <div class="callout callout-info">
+    <h5>Selamat Datang!</h5>
+
+    <p>Sistem Informasi Pemesana Njenggrik Coffee</p>
+  </div>
+</section>
+
 @endsection
